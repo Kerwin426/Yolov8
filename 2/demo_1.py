9adf15068b3d70fed05499fdae3d0d72a8da3eca
Kerwin426 <2901997993@qq.com>
2024-07-28 01:11:51 +0800

    训练完了，内存差点爆炸

--- 2/demo_1.py
+++ 2/demo_1.py
@@ -9,7 +9,7 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 def train():
     # model = YOLO("./ultralytics/cfg/models/v8/mtyolov8.yaml")
     model = YOLO("./yolov8n.pt")
-    model.train(data="./ultralytics/cfg/datasets/mt.yaml", epochs=20)
+    model.train(data="../ultralytics-main/ultralytics/cfg/datasets/mt.yaml", epochs=20)
     result = model.val()
 
 if __name__ == '__main__':
